fad333dacc7dce2014bbbb0f4f296ad9cc9fb95a
bubkoo <bubkoo.wy@gmail.com>
2020-09-25 17:39:53 +0800

    feat: ✨ init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# github-app-token
-Impersonate a GitHub App in a GitHub Action
+# Use App Token
+
+This GitHub Action can be used to impersonate a GitHub App when `secrets.GITHUB_TOKEN`'s limitations are too restrictive and a personal access token is not suitable.
+
+[`secrets.GITHUB_TOKEN`](https://help.github.com/en/actions/configuring-and-managing-workflows/authenticating-with-the-github_token) has limitations such as [not being able to triggering a new workflow from another workflow](https://github.community/t5/GitHub-Actions/Triggering-a-new-workflow-from-another-workflow/td-p/31676). A workaround is to use a [personal access token](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line) from a [personal user/bot account](https://help.github.com/en/github/getting-started-with-github/types-of-github-accounts#personal-user-accounts). However, for organizations, GitHub Apps are [a more appropriate automation solution](https://developer.github.com/apps/differences-between-apps/#machine-vs-bot-accounts).
+
+# Example Workflow
+
+```yml
+jobs:
+  job:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Generate token
+        id: generate_token
+        uses: bubkoo/use-app-token@v1
+        with:
+          APP_ID: ${{ secrets.APP_ID }}
+          PRIVATE_KEY: ${{ secrets.PRIVATE_KEY }}
+      - name: Use token
+        env:
+          TOKEN: ${{ steps.generate_token.outputs.token }}
+        run: |
+          echo "The generated token is masked: ${TOKEN}"
+```
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { App } from '@octokit/app'
+import isBase64 from 'is-base64'
+import sodium from 'tweetsodium'
+
+async function run() {
+  try {
+    const id = Number(core.getInput('APP_ID', { required: true }))
+    const privateKeyInput = core.getInput('PRIVATE_KEY', { required: true })
+    const privateKey = isBase64(privateKeyInput)
+      ? Buffer.from(privateKeyInput, 'base64').toString('utf8')
+      : privateKeyInput
+    const app = new App({ id, privateKey })
+    const jwt = app.getSignedJsonWebToken()
+    const octokit = github.getOctokit(jwt)
+    const {
+      data: { id: installationId },
+    } = await octokit.apps.getRepoInstallation(github.context.repo)
+    const token = await app.getInstallationAccessToken({
+      installationId,
+    })
+
+    await createOrUpdateTokenInSecret(octokit, token)
+    core.setSecret(token)
+    core.setOutput('token', token)
+    core.info('Token generated successfully!')
+  } catch (e) {
+    core.error(e)
+    core.setFailed(e.message)
+  }
+}
+
+function encryptSecret(value: string) {
+  const key = 'base64-encoded-public-key'
+
+  // Convert the message and key to Uint8Array's (Buffer implements that interface)
+  const messageBytes = Buffer.from(value)
+  const keyBytes = Buffer.from(key, 'base64')
+
+  // Encrypt using LibSodium.
+  const encryptedBytes = sodium.seal(messageBytes, keyBytes)
+
+  // Base64 the encrypted secret
+  return Buffer.from(encryptedBytes).toString('base64')
+}
+
+async function createOrUpdateTokenInSecret(
+  octokit: ReturnType<typeof github.getOctokit>,
+  token: string,
+) {
+  try {
+    const name = core.getInput('SECRET_NAME')
+    if (name) {
+      return octokit.actions.createOrUpdateRepoSecret({
+        ...github.context.repo,
+        secret_name: name,
+        encrypted_value: encryptSecret(token),
+      })
+    }
+  } catch (error) {
+    core.error(error)
+  }
+}
+
+void run()
